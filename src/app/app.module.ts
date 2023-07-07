5ec7007959b722b54fcfb6d7b89db50eb9aaa816
Nikhil Bankar <bankarniks7@gmail.com>
2023-07-07 14:35:40 +0530

    adding modified component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,4 +17,4 @@ import { WarningalertComponent } from './warningalert/warningalert.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+ export class AppModule { }
