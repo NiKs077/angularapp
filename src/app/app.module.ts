db8084a463f41c8481b95a6ade0f9b1dfc0f271e
Nikhil Bankar <bankarniks7@gmail.com>
2023-07-07 12:56:34 +0530

    added two new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessalertComponent,
+    WarningalertComponent
   ],
   imports: [
     BrowserModule
